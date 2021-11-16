2a3912424ae4bbf6ec2fcceeb89b71bc9a2076df
UK <uk.kdw8794@gmail.com>
2021-11-16 23:27:05 +0900

    UK

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    // TEST
+    fun btnClick(view: android.view.View) {
+        Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
